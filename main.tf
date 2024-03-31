782b08e97b146fa79eaa0ca0c6ab077602122ea2
SankaW <sdweerathunga5@gmail.com>
2024-03-31 19:25:07 -0400

    Initial commit

--- main.tf
+++ main.tf
@@ -19,9 +19,18 @@ resource "aws_s3_bucket" "hosting_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "hosting_bucket_acl" {
-    bucket = aws_s3_bucket.hosting_bucket.id
-    acl = "public-read"
+# resource "aws_s3_bucket_acl" "hosting_bucket_acl" {
+#     bucket = aws_s3_bucket.hosting_bucket.id
+#     acl = "public-read"
+# }
+
+resource "aws_s3_bucket_public_access_block" "hosting_bucket_public_access" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+  
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
